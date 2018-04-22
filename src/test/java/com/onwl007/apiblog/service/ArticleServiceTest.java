c7db30f6696382de8c97ad863f7cd47e0d75d0d7
onwl007 <onwl007@126.com>
2018-04-22 21:06:54 +0800

    unit test

---
+++ src/test/java/com/onwl007/apiblog/service/ArticleServiceTest.java
@@ -0,0 +1,16 @@
+package com.onwl007.apiblog.service;
+
+import com.onwl007.apiblog.ApiBlogApplication;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author onwl007@126.com
+ * @date 2018/4/22 20:48
+ * @desc unit test
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ApiBlogApplication.class)
+public class ArticleServiceTest {
+}
