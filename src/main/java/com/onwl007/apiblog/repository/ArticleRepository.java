ab9a61fdb914346d525fd3a665ee169318bd50fb
onwl007 <onwl007@126.com>
2018-04-22 20:08:35 +0800

    ArticleRepository

---
+++ src/main/java/com/onwl007/apiblog/repository/ArticleRepository.java
@@ -0,0 +1,42 @@
+package com.onwl007.apiblog.repository;
+
+
+import com.onwl007.apiblog.domain.Article;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.List;
+
+/**
+ * @author onwl007@126.com
+ * @date 2018/4/22 15:23
+ * @desc
+ */
+public interface ArticleRepository extends MongoRepository<Article,String>{
+
+    /**
+     * 分页查询
+     * @param pageable
+     * @return
+     */
+    Page<Article> findAll(Pageable pageable);
+
+    /**
+     * 根据创建时间降序排列
+     * @return
+     */
+    List<Article> findAllByOrderByCreatedAtDesc();
+
+    /**
+     * 根据文章id查询为文章
+     * @return
+     */
+    Article findArticleById(String id);
+
+    /**
+     * 根据文章标题查询
+     * @param title
+     * @return
+     */
+    Article findByTitle(String title);
+}
