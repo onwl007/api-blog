d3639025605ff541fcd81eceb12fe7e19cd5c004
onwl007 <onwl007@126.com>
2018-04-22 20:09:01 +0800

    ArticleService

---
+++ src/main/java/com/onwl007/apiblog/service/ArticleService.java
@@ -0,0 +1,52 @@
+package com.onwl007.apiblog.service;
+
+import com.onwl007.apiblog.domain.Article;
+import com.onwl007.apiblog.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author onwl007@126.com
+ * @date 2018/4/22 16:23
+ * @desc
+ */
+@Service
+public class ArticleService {
+
+    @Autowired
+    ArticleRepository articleRepository;
+
+    /**
+     * 查询所有文章按照创建时间降序排列
+     * @return
+     */
+    public List<Article> listArticles(){
+        return articleRepository.findAllByOrderByCreatedAtDesc();
+    }
+
+    /**
+     * 根据id查询文章
+     * @return
+     */
+    public Article getArticleById(String id){
+        return articleRepository.findArticleById(id);
+    }
+
+    /**
+     * 创建文章或更新文章
+     * @param article
+     */
+    public void createArticle(Article article){
+        articleRepository.save(article);
+    }
+
+    /**
+     * 删除文章
+     */
+    public void deleteArticle(String id){
+        articleRepository.deleteById(id);
+    }
+
+}
