1b68665024c18d858326b4bf1213029e0ea764af
onwl007 <onwl007@126.com>
2018-04-22 20:21:38 +0800

    CategoryRepository

---
+++ src/main/java/com/onwl007/apiblog/repository/CategoryRepository.java
@@ -0,0 +1,19 @@
+package com.onwl007.apiblog.repository;
+
+import com.onwl007.apiblog.domain.Category;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+/**
+ * @author onwl007@126.com
+ * @date 2018/4/22 20:16
+ * @desc
+ */
+public interface CategoryRepository extends MongoRepository<Category,String> {
+    /**
+     * 根据list升序排列
+     * @return
+     */
+    List<Category> findAllByOrderByListAsc();
+}
