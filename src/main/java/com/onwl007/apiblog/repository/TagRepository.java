aa9e7cd5fced2b501eefe67700815239b9d3fcf4
onwl007 <onwl007@126.com>
2018-04-23 21:47:55 +0800

    TagRepository

---
+++ src/main/java/com/onwl007/apiblog/repository/TagRepository.java
@@ -0,0 +1,27 @@
+package com.onwl007.apiblog.repository;
+
+import com.onwl007.apiblog.domain.Tag;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+/**
+ * @author onwl007@126.com
+ * @date 2018/4/23 21:36
+ * @desc
+ */
+public interface TagRepository extends MongoRepository<Tag,String>{
+
+    /**
+     * 根据创建时间降序排列
+     * @return
+     */
+    List<Tag> findAllByOrderByCreateAtDesc();
+
+    /**
+     * 根据标签id查询
+     * @param id
+     * @return
+     */
+    Tag findTagById(String id);
+}
