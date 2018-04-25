8bac4adc004f92d2d5bd30ff1a44ec742f1f1ba5
onwl007 <onwl007@126.com>
2018-04-25 21:24:04 +0800

    OptionService

--- src/main/java/com/onwl007/apiblog/service/OptionService.java
+++ src/main/java/com/onwl007/apiblog/service/OptionService.java
@@ -1,7 +1,9 @@
 package com.onwl007.apiblog.service;
 
+import com.onwl007.apiblog.domain.Option;
 import com.onwl007.apiblog.repository.OptionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
 /**
@@ -14,4 +16,21 @@ public class OptionService {
 
     @Autowired
     OptionRepository optionRepository;
+
+    /**
+     * 保存或更新option
+     * @param option
+     */
+    public void saveOption(Option option){
+        optionRepository.save(option);
+    }
+
+
+    /**
+     * 查询一条数据
+     * @return
+     */
+    public Option getOption(){
+        return optionRepository.findAll().get(0);
+    }
 }
