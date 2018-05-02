d26cbd9a9fd5536ca26f79b840018d0b371582a9
onwl007 <onwl007@126.com>
2018-05-02 21:11:36 +0800

    modify

--- src/main/java/com/onwl007/apiblog/repository/ArticleRepository.java
+++ src/main/java/com/onwl007/apiblog/repository/ArticleRepository.java
@@ -5,6 +5,7 @@ import com.onwl007.apiblog.domain.Article;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
 import java.util.List;
 
 /**
@@ -12,10 +13,11 @@ import java.util.List;
  * @date 2018/4/22 15:23
  * @desc
  */
-public interface ArticleRepository extends MongoRepository<Article,String>{
+public interface ArticleRepository extends MongoRepository<Article, String> {
 
     /**
      * 分页查询
+     *
      * @param pageable
      * @return
      */
@@ -23,20 +25,33 @@ public interface ArticleRepository extends MongoRepository<Article,String>{
 
     /**
      * 根据创建时间降序排列
+     *
      * @return
      */
     List<Article> findAllByOrderByCreatedAtDesc();
 
     /**
      * 根据文章id查询为文章
+     *
      * @return
      */
     Article findArticleById(String id);
 
     /**
      * 根据文章标题查询
+     *
      * @param title
      * @return
      */
     Article findByTitle(String title);
+
+    /**
+     * 查询关联了同一个标签的所有的文章
+     *
+     * @param id
+     * @return
+     */
+    Page<Article> findAllByTag_Id(String id,Pageable pageable);
+
+    Page<Article> findAllByCategory_Id(String id,Pageable pageable );
 }
--- src/main/java/com/onwl007/apiblog/service/ArticleService.java
+++ src/main/java/com/onwl007/apiblog/service/ArticleService.java
@@ -3,11 +3,14 @@ package com.onwl007.apiblog.service;
 import com.onwl007.apiblog.domain.Article;
 import com.onwl007.apiblog.repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.aggregation.Aggregation;
 import org.springframework.stereotype.Service;
 
+import java.security.PublicKey;
 import java.util.List;
 
 /**
@@ -25,49 +28,83 @@ public class ArticleService {
     private MongoTemplate mongoTemplate;
 
     /**
-     * 查询所有文章按照创建时间降序排列
+     * 分页查询
+     *
+     * @param pageable
      * @return
      */
-    public List<Article> listArticles(){
+    public Page<Article> pageArticles(Pageable pageable) {
+        return articleRepository.findAll(pageable);
+    }
+
+    /**
+     * 查询所有文章按照创建时间降序排列
+     *
+     * @return
+     */
+    public List<Article> listArticles() {
         return articleRepository.findAllByOrderByCreatedAtDesc();
     }
 
     /**
      * 根据id查询文章
+     *
      * @return
      */
-    public Article getArticleById(String id){
+    public Article getArticleById(String id) {
         return articleRepository.findArticleById(id);
     }
 
     /**
+     * 查询关联同一个标签下的所有文章
+     *
+     * @param id
+     * @param pageable
+     * @return
+     */
+    public Page<Article> getArticleByTagId(String id, Pageable pageable) {
+        return articleRepository.findAllByTag_Id(id, pageable);
+    }
+
+    /**
+     * 查询关联同一个分类下的所有文章
+     *
+     * @param id
+     * @param pageable
+     * @return
+     */
+    public Page<Article> getArticleByCategoryId(String id, Pageable pageable) {
+        return articleRepository.findAllByCategory_Id(id, pageable);
+    }
+
+    /**
      * 创建文章或更新文章
+     *
      * @param article
      */
-    public void createArticle(Article article){
+    public void createArticle(Article article) {
         articleRepository.save(article);
     }
 
     /**
      * 删除文章
      */
-    public void deleteArticle(String id){
+    public void deleteArticle(String id) {
         articleRepository.deleteById(id);
     }
 
     /**
-     *
      * @param sort
      * @return
      */
-    public List<Article> listHotArticle(Sort sort){
+    public List<Article> listHotArticle(Sort sort) {
         return articleRepository.findAll(sort);
     }
 
     /**
      * 查询归档文章
      */
-    public void archivesArticle(){
+    public void archivesArticle() {
 
     }
 
