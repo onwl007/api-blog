ffd5907678cf1944a61fbe03dd7dbd124da724b8
onwl007 <onwl007@126.com>
2018-06-02 20:10:56 +0800

    modify

--- src/main/java/com/onwl007/apiblog/service/UserService.java
+++ src/main/java/com/onwl007/apiblog/service/UserService.java
@@ -1,10 +1,13 @@
 package com.onwl007.apiblog.service;
 
+import com.onwl007.apiblog.domain.Comment;
 import com.onwl007.apiblog.domain.User;
+import com.onwl007.apiblog.repository.CommentRepository;
 import com.onwl007.apiblog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,6 +20,9 @@ public class UserService {
 
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    CommentRepository commentRepository;
 
     /**
      * 查询所有用户
@@ -73,5 +79,20 @@ public class UserService {
      */
     public void deleteUser(String id) {
         userRepository.deleteById(id);
+    }
+
+    /**
+     * 统计站内留言的用户的个数
+     *
+     * @return
+     */
+    public List<User> getUsersGuest() {
+        List<Comment> commentList = commentRepository.findAllByType(1);
+        List<User> userList = new ArrayList<>();
+        for (int i = 0; i < commentList.size(); i++) {
+            User user = commentList.get(i).getAuthor();
+            userList.add(user);
+        }
+        return userList;
     }
 }
