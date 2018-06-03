aa89fdf6039d15147dc2f0b76e0613c165cd364d
onwl007 <onwl007@126.com>
2018-06-03 22:15:57 +0800

    archives

--- src/main/java/com/onwl007/apiblog/controller/ArticleController.java
+++ src/main/java/com/onwl007/apiblog/controller/ArticleController.java
@@ -1,5 +1,7 @@
 package com.onwl007.apiblog.controller;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import com.onwl007.apiblog.domain.Article;
 import com.onwl007.apiblog.domain.RestResult;
 import com.onwl007.apiblog.page.ArticlePagination;
@@ -11,7 +13,7 @@ import com.onwl007.apiblog.util.CodeMap;
 import com.onwl007.apiblog.util.MongoUtil;
 import com.onwl007.apiblog.util.ResultGenerator;
 import com.onwl007.apiblog.util.ServiceException;
-import com.onwl007.apiblog.vo.ArticleMeta;
+import com.onwl007.apiblog.vo.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -19,9 +21,15 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
+import javax.print.DocFlavor;
 import javax.servlet.http.HttpServletRequest;
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * @author onwl007@126.com
@@ -118,8 +126,8 @@ public class ArticleController {
             throw new ServiceException("参数错误");
         }
         Article article = articleService.getArticleById(id);
-        ArticleMeta meta=article.getMeta();
-        int pvs=meta.getPvs()+1;
+        ArticleMeta meta = article.getMeta();
+        int pvs = meta.getPvs() + 1;
         meta.setPvs(pvs);
         article.setMeta(meta);
         articleService.createArticle(article);
@@ -164,8 +172,59 @@ public class ArticleController {
         return generator.getFailResult();
     }
 
+    /**
+     * 文章归档
+     *
+     * @return
+     * @throws ParseException
+     */
     @GetMapping("/archives")
-    public RestResult getArticleArchives() {
+    public RestResult getArticleArchives() throws ParseException {
+        List<JsonObject> list = articleService.archivesArticle();
+        List<DataArchivesVO> data = new ArrayList<>();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        for (int i = 0; i < list.size(); i++) {
+            JsonObject jsonObject = list.get(i);
+            String year = jsonObject.getAsJsonObject("_id").get("year").getAsString();
+            String month = jsonObject.getAsJsonObject("_id").get("month").getAsString();
+            JsonArray array = jsonObject.getAsJsonArray("article");
+            List<ArticlesVO> articlesVOList = new ArrayList<>();
+            for (int j = 0; j < array.size(); j++) {
+                String title = array.get(j).getAsJsonObject().get("title").getAsString();
+                String _id = array.get(j).getAsJsonObject().get("_id").getAsJsonObject().get("$oid").getAsString();
+                String date = array.get(j).getAsJsonObject().get("createAt").getAsJsonObject().get("$date").getAsString().substring(0, 10);
+                Long timestamp = Long.parseLong(date) * 1000;
+                String time = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(timestamp));
+                Date createAt = dateFormat.parse(time);
+                ArticlesVO articlesVO = new ArticlesVO(_id, title, createAt);
+                articlesVOList.add(articlesVO);
+            }
+            DataArchivesVO dataArchivesVO = new DataArchivesVO(year, month, articlesVOList);
+            data.add(dataArchivesVO);
+        }
+
+        if (data != null && data.size() > 0) {
+            List<ListVO> listVOS = new ArrayList<>();
+            int count = 0;
+            for (int i = 0; i < data.size(); i++) {
+                List<MonthsVO> monthsVOList = new ArrayList<>();
+                SimpleDateFormat mFormat = new SimpleDateFormat("MM");
+                count += data.get(i).getArticles().size();
+                String year = data.get(i).getYear();
+                String month = data.get(i).getMonth();
+                Date date = mFormat.parse(month);
+                mFormat = new SimpleDateFormat("MMMM", Locale.ENGLISH);
+                String monthStr = mFormat.format(date);
+                List<ArticlesVO> articlesVOList = data.get(i).getArticles();
+                MonthsVO monthsVO = new MonthsVO(month, monthStr, articlesVOList);
+                monthsVOList.add(monthsVO);
+                ListVO listVO = new ListVO(year, monthsVOList);
+                listVOS.add(listVO);
+            }
+
+            ArchivesVO archivesVO = new ArchivesVO(count, listVOS);
+            return generator.getSuccessResult("获取文章归档成功", archivesVO);
+        }
         return generator.getFailResult();
     }
 
