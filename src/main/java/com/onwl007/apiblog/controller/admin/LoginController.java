116d2555b23f7e493c3e1e5d7d1d54d3cdc1a7c6
onwl007 <onwl007@126.com>
2018-06-11 20:56:49 +0800

    modify

--- src/main/java/com/onwl007/apiblog/controller/admin/LoginController.java
+++ src/main/java/com/onwl007/apiblog/controller/admin/LoginController.java
@@ -3,7 +3,6 @@ package com.onwl007.apiblog.controller.admin;
 import com.onwl007.apiblog.domain.RestResult;
 import com.onwl007.apiblog.domain.TokenModel;
 import com.onwl007.apiblog.domain.User;
-import com.onwl007.apiblog.repository.TokenManager;
 import com.onwl007.apiblog.repository.UserRepository;
 import com.onwl007.apiblog.util.JWT;
 import com.onwl007.apiblog.util.ResultGenerator;
@@ -13,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 /**
@@ -33,16 +34,25 @@ public class LoginController {
 
     private String str;
 
+    private String name;
+
     @PostMapping("/auth/local/login")
-    public RestResult login(@RequestBody Map<String, Object> map){
+    public RestResult login(@RequestBody Map<String, Object> map, HttpServletResponse response, HttpServletRequest request){
         String username=map.get("name").toString();
+        name=username;
         String password=map.get("password").toString();
         User user=userRepository.findUserByName(username);
         if (user==null || !user.getPassword().equals(password)){
             return generator.getFailResult("用户名或密码错误",null);
         }
         LoginVO loginVO=new LoginVO(user.getId(),user.getName());
-        String token= JWT.sign(loginVO,60000L*60L*24L*7L);
+        String token= JWT.sign(loginVO,60000);
+        System.out.println(token);
+        Cookie cookie=new Cookie("jooger.me.token",token);
+        cookie.setHttpOnly(false);
+        cookie.setMaxAge(0);
+        cookie.setSecure(false);
+        cookie.setDomain("jooger.me.userId"+user.getId());
         str=token;
         TokenModel model=new TokenModel(user.getId(),token);
         return generator.getSuccessResult("登录成功",model);
@@ -50,7 +60,7 @@ public class LoginController {
 
     @GetMapping("/auth/info")
     public RestResult loginInfo(){
-        User user=userRepository.findUserByName("Jooger");
+        User user=userRepository.findUserByName(name);
         AuthInfoVO authInfoVO=new AuthInfoVO(user,str);
         return generator.getSuccessResult("请求成功",authInfoVO);
     }
