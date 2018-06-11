fff26febdce25f201cfe3abb1198e26f6c2a9693
onwl007 <onwl007@126.com>
2018-06-11 20:56:08 +0800

    create

---
+++ src/main/java/com/onwl007/apiblog/config/Constants.java
@@ -0,0 +1,24 @@
+package com.onwl007.apiblog.config;
+
+/**
+ * @author onwl007@126.com
+ * @date 2018/6/10 21:06
+ * @desc 常量
+ */
+public class Constants {
+
+    /**
+     * 存储当前登录用户id的字段名
+     */
+    public static final String CURRENT_USER_ID = "CURRENT_USER_ID";
+
+    /**
+     * token有效期（小时）
+     */
+    public static final int TOKEN_EXPIRES_HOUR = 72;
+
+    /**
+     * 存放Authorization的header字段
+     */
+    public static final String AUTHORIZATION = "authorization";
+}
