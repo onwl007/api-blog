6ea01e8ba212db6717d5cd86036045ac73216456
onwl007 <onwl007@126.com>
2018-07-04 23:16:29 +0800

    modify

--- src/main/java/com/onwl007/apiblog/repository/ArticleRepository.java
+++ src/main/java/com/onwl007/apiblog/repository/ArticleRepository.java
@@ -6,6 +6,7 @@ import com.onwl007.apiblog.domain.Tag;
 import com.onwl007.apiblog.page.Pagination;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
@@ -18,7 +19,7 @@ import java.util.List;
 public interface ArticleRepository extends MongoRepository<Article, String> {
 
     /**
-     * 分页查询
+     * 后台查询所有文章
      *
      * @param pageable
      * @return
@@ -28,7 +29,7 @@ public interface ArticleRepository extends MongoRepository<Article, String> {
     /**
      * 前台查询已发布的文章
      *
-     * @param state
+     * @param state    1 已发布 | 0 未发布
      * @param pageable
      * @return
      */
@@ -40,6 +41,15 @@ public interface ArticleRepository extends MongoRepository<Article, String> {
      * @return
      */
     List<Article> findAllByOrderByCreateAtDesc();
+
+    /**
+     * 根据文章的发布状态查询热门文章
+     *
+     * @param state 1 已发布 | 0 未发布
+     * @param sort  排序
+     * @return
+     */
+    List<Article> findAllByStateEquals(int state, Sort sort);
 
     /**
      * 根据文章id查询为文章
@@ -58,12 +68,24 @@ public interface ArticleRepository extends MongoRepository<Article, String> {
 
     /**
      * 对文章标题进行模糊查询
+     * 查询所有文章
      *
      * @param key
      * @param pageable
      * @return
      */
     Page<Article> findArticleByTitleLike(String key, Pageable pageable);
+
+    /**
+     * 对文章标题进行模糊查询
+     * 查询已发布文章
+     *
+     * @param key      标题
+     * @param state    1 已发布 | 0 未发布
+     * @param pageable
+     * @return
+     */
+    Page<Article> findArticleByTitleLikeAndStateEquals(String key, int state, Pageable pageable);
 
     /**
      * 查询关联了同一个标签的所有文章
@@ -75,13 +97,23 @@ public interface ArticleRepository extends MongoRepository<Article, String> {
     Page<Article> findAllByTagIn(Tag[] tag, Pageable pageable);
 
     /**
-     * 查询关联了同一个分类下的所有文章
+     * 后台查询关联了同一个分类下的所有文章
      *
      * @param id
      * @param pageable
      * @return
      */
     Page<Article> findAllByCategory_Id(String id, Pageable pageable);
+
+    /**
+     * 前台查询关联了同一个分类下的已发布文章
+     *
+     * @param id       分类ID
+     * @param state    1 已发布 | 0 未发布
+     * @param pageable
+     * @return
+     */
+    Page<Article> findAllByCategory_IdAndStateEquals(String id, int state, Pageable pageable);
 
     /**
      * 根据文章标题查询文章
