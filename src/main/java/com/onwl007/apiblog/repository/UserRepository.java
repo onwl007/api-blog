4c25bd522164086e47b0e53b24cb0b899f75b5cb
onwl007 <onwl007@126.com>
2018-08-04 23:41:41 +0800

    findByName

--- src/main/java/com/onwl007/apiblog/repository/UserRepository.java
+++ src/main/java/com/onwl007/apiblog/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.onwl007.apiblog.repository;
 import com.onwl007.apiblog.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 /**
  * @author onwl007@126.com
  * @date 2018/4/23 22:52
@@ -33,5 +35,7 @@ public interface UserRepository extends MongoRepository<User, String> {
      * @return
      */
     User findUserByName(String name);
+
+    Optional<User> findByName(String name);
 
 }
