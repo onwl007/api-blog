d25b2c41bf4582967208d72d307d882f82d8f6ab
onwl007 <onwl007@126.com>
2018-08-04 23:44:29 +0800

    修改为 token 登录

--- src/main/java/com/onwl007/apiblog/controller/admin/LoginController.java
+++ src/main/java/com/onwl007/apiblog/controller/admin/LoginController.java
@@ -1,20 +1,29 @@
 package com.onwl007.apiblog.controller.admin;
 
+import com.onwl007.apiblog.config.WebSecurityConfig;
 import com.onwl007.apiblog.domain.RestResult;
 import com.onwl007.apiblog.domain.TokenModel;
 import com.onwl007.apiblog.domain.User;
 import com.onwl007.apiblog.repository.UserRepository;
 import com.onwl007.apiblog.util.JWT;
+import com.onwl007.apiblog.util.JWTTokenUtils;
 import com.onwl007.apiblog.util.ResultGenerator;
 import com.onwl007.apiblog.vo.AuthInfoVO;
 import com.onwl007.apiblog.vo.LoginVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * @author onwl007@126.com
@@ -32,30 +41,39 @@ public class LoginController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JWTTokenUtils jwtTokenUtils;
+
     private String str;
 
     private String name;
 
     @PostMapping("/auth/local/login")
-    public RestResult login(@RequestBody Map<String, Object> map, HttpServletResponse response, HttpServletRequest request){
+    public RestResult login(@RequestBody Map<String, Object> map, HttpServletResponse response, HttpServletRequest request) throws Exception{
         String username=map.get("name").toString();
         name=username;
         String password=map.get("password").toString();
-        User user=userRepository.findUserByName(username);
-        if (user==null || !user.getPassword().equals(password)){
-            return generator.getFailResult("用户名或密码错误",null);
+
+        UsernamePasswordAuthenticationToken authenticationToken=new UsernamePasswordAuthenticationToken(username,password);
+        if (Objects.nonNull(authenticationToken)){
+            userRepository.findByName(authenticationToken.getPrincipal().toString())
+                    .orElseThrow(()->new Exception("用户不存在"));
         }
-        LoginVO loginVO=new LoginVO(user.getId(),user.getName());
-        String token= JWT.sign(loginVO,60000);
-        System.out.println(token);
-        Cookie cookie=new Cookie("jooger.me.token",token);
-        cookie.setHttpOnly(false);
-        cookie.setMaxAge(0);
-        cookie.setSecure(false);
-        cookie.setDomain("jooger.me.userId"+user.getId());
-        str=token;
-        TokenModel model=new TokenModel(user.getId(),token);
-        return generator.getSuccessResult("登录成功",model);
+        try {
+            User user=userRepository.findUserByName(username);
+            Authentication authentication=authenticationManager.authenticate(authenticationToken);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            String token=jwtTokenUtils.createToken(authentication,false,user.getId());
+            response.addHeader(WebSecurityConfig.AUTHORIZATION_HEADER,token);
+            str=token;
+            TokenModel model=new TokenModel(user.getId(),token);
+            return generator.getSuccessResult("登录成功",model);
+        }catch (BadCredentialsException authentication){
+            throw new Exception("密码错误");
+        }
     }
 
     @GetMapping("/auth/info")
